26612d603d94dd72eb726810795111a635c82636
Luis Bravo <luisbravo1604@gmail.com>
2024-02-14 02:17:25 -0500

    0.4

--- iotriegoweb/build.sh
+++ iotriegoweb/build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 # exit on error*-
 set -o errexit
-pip install --upgrade pip
-pip install --force-reinstall -U setuptools
-pip install --upgrade pippip install --force-reinstall -U setuptools
-pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
